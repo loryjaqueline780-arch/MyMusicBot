11c4bf9d5258a5efef2f577f042624bf0e121e28
loryjaqueline780-arch <loryjaqueline780@gmail.com>
2026-08-29 11:20:38 +0500

    Add files via upload

--- music_bot.py
+++ music_bot.py
@@ -1,3 +1,6 @@
+import asyncio
+asyncio.set_event_loop(asyncio.new_event_loop())
+
 import os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
